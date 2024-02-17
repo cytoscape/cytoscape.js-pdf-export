ea48c04b73292ce1ebae2ca4b46836a569ce7338
Mike Kucera <mikekucera@gmail.com>
2024-02-17 11:55:05 -0500

    Switch from rollup to parcel for builds

--- src/pdf-export.js
+++ src/pdf-export.js
@@ -1,6 +1,30 @@
+import canvas2pdf from "canvas2pdf";
+import blobStream from "blob-stream";
+
+import PDFDocument from 'pdfkit';
+
 
 export function pdfExport(options) {
-  console.log("pdf export!!!");
+  console.log("pdf export ???");
+
+  window.PDFDocument = PDFDocument;
+  window.blobStream = blobStream;
+
+  const ctx = new canvas2pdf.PdfContext(blobStream());
+
+
+
+  // // draw your canvas like you would normally
+  // ctx.fillStyle = "yellow";
+  // ctx.fillRect(100, 100, 100, 100);
+  // // more canvas drawing, etc...
+
+  // // convert your PDF to a Blob and save to file
+  // ctx.stream.on("finish", function () {
+  //   const blob = ctx.stream.toBlob("application/pdf");
+  //   saveAs(blob, "example.pdf", true);
+  // });
+  // ctx.end();
 }
 
 export default pdfExport;
