709aa8952141dc0b25a51f1936f1dbd02a993d35
Mike Kucera <mikekucera@gmail.com>
2024-02-21 16:19:36 -0500

    Switch from parcel to webpack :)

--- src/pdf-export.js
+++ src/pdf-export.js
@@ -1,18 +1,34 @@
+import PDFDocument from "pdfkit";
 import canvas2pdf from "canvas2pdf";
 import blobStream from "blob-stream";
+import saveAs from "file-saver";
 
-import PDFDocument from 'pdfkit';
+
+// Required by pdfkit webpack config or whatever I dunno
+// https://github.com/blikblum/pdfkit-webpack-example/issues/1
+import './registerPdfkitStaticFiles.js';
 
 
-export default function pdfExport(options) {
+export default function register(cytoscape) {
+  if(!cytoscape) { return; }
+  cytoscape('core', 'pdf', pdfExport);
+};
+
+if(typeof cytoscape !== 'undefined') { // expose to global cytoscape (i.e. window.cytoscape)
+  register(cytoscape);
+}
+
+// Housekeeping to make pdfkit happy
+window.PDFDocument = PDFDocument;
+window.blobStream = blobStream;
+
+
+
+export function pdfExport(options) {
   console.log("pdf export ???");
   const cy = this;
 
-  window.PDFDocument = PDFDocument;
-  window.blobStream = blobStream;
-
   const ctx = new canvas2pdf.PdfContext(blobStream());
-
   console.log(ctx);
 
   bufferCanvasImage(cy, ctx, {});
@@ -23,12 +39,12 @@ export default function pdfExport(options) {
   // ctx.fillRect(100, 100, 100, 100);
   // // more canvas drawing, etc...
 
-  // // convert your PDF to a Blob and save to file
-  // ctx.stream.on("finish", function () {
-  //   const blob = ctx.stream.toBlob("application/pdf");
-  //   saveAs(blob, "example.pdf", true);
-  // });
-  // ctx.end();
+  // convert your PDF to a Blob and save to file
+  ctx.stream.on("finish", function () {
+    const blob = ctx.stream.toBlob("application/pdf");
+    saveAs(blob, "example.pdf", true);
+  });
+  ctx.end();
 }
 
 const isNumber = obj => obj != null && typeof obj === typeof 1 && !isNaN( obj );
