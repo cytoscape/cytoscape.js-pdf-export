0032ea7512e6363d941f2230b799e74dab013d49
Mike Kucera <mikekucera@gmail.com>
2024-02-28 13:24:20 -0500

    Add node types test

---
+++ demos/node-types/code.js
@@ -0,0 +1,23 @@
+/* global document, window, fetch, cytoscape */
+
+(function(){
+  var toJson = function(res){ return res.json(); };
+
+  const cy = cytoscape({
+    container: document.getElementById('cy'),
+
+    layout: {
+      name: 'grid'
+    },
+
+    style: fetch('cy-style.json').then(toJson),
+
+    elements: fetch('data.json').then(toJson)
+  });
+
+  cy.ready(() => {
+    window.pdf = () => cy.pdf();
+  });
+
+  window.cy = cy;
+})();
