8d957914ba41d274428b003d805754c5d2b0eb31
Mike Kucera <mikekucera@gmail.com>
2024-03-27 14:43:01 -0400

    create common-js module

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: pkg.name + '.js',
+    libraryTarget: "commonjs-module",
   },
   plugins: [
     // new HtmlWebpackPlugin({
